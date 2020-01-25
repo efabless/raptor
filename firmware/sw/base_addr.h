9aa3207a7cec8210fdec70f716c4e76edf74e4e2
jeffdi <jeffdi@efabless.com>
2020-01-24 23:48:32 -0800

    initial commit

---
+++ firmware/sw/base_addr.h
@@ -0,0 +1,27 @@
+#ifndef NULL
+    #define NULL 0
+#endif
+#define INVALID_ADDR NULL
+
+#define     APB_BASE_ADDR_0             0x0fa00000
+#define     CLK_CTRL_BASE_ADDR_0        0x13880000
+#define     I2C_BASE_ADDR_0             0x40100000
+#define     UART_BASE_ADDR_0            0x40200000
+#define     UART_BASE_ADDR_1            0x40300000
+#define     APB_SPI_BASE_ADDR_0         0x40400000
+#define     PWM_BASE_ADDR_0             0x40500000
+#define     PWM_BASE_ADDR_1             0x40600000
+#define     ADC_BASE_ADDR_0             0x40700000
+#define     ADC_BASE_ADDR_1             0x40800000
+#define     DAC_BASE_ADDR_0             0x40900000
+#define     COMPARATOR_BASE_ADDR_0      0x40a00000
+#define     AHB_SPI_BASE_ADDR_1         0x70000000
+#define     AHB_GPIO_BASE_ADDR_0        0x80000000
+
+#define     APB_GPIO_BASE_ADDR_0		INVALID_ADDR
+
+#define     TIMER_BASE_ADDR_0      		INVALID_ADDR
+
+#define     BG_BASE_ADDR_0				INVALID_ADDR
+
+#define     DBGIO_BASE_ADDR_0           0x24000000
