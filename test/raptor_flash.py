4228fa7ade7773ca9d3501ae089717cb4894b7f5
jeffdi <jeffdi@efabless.com>
2020-01-27 13:54:54 -0800

    raptor_flash.py added

--- test/raptor_flash.py
+++ test/raptor_flash.py
@@ -1,7 +1,15 @@
 from pyftdi.ftdi import Ftdi
+import time
+from pyftdi.spi import SpiController
+from array import array as Array
 
-Ftdi().open_mpsse_from_url('ftdi://ftdi:232h:2:6/1')
-print(Ftdi().has_mpsse)
-print(Ftdi().is_mpsse)
+ctrl = SpiController(cs_count=1, turbo=True)
+ctrl.configure(vendor=0x0403, product=0x6014, interface=1)
+spi = ctrl.get_port(cs=0)  # Chip select is 0 -- corresponds to D3
+spi.set_frequency(3000000)
 
-Ftdi().close()
+# acbus_direction = 0x01 # Bit C0 is output for reset
+# ctrl._ftdi.write_data(Array('B', [Ftdi.SET_BITS_HIGH, 0x00, acbus_direction]))
+# time.sleep(1.0)
+# ctrl._ftdi.write_data(Array('B', [Ftdi.SET_BITS_HIGH, 0x01, acbus_direction]))
+# time.sleep(1.0)
