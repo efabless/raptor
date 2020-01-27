66088856e61f26dd3da70c1a99a619926e36a3da
jeffdi <jeffdi@efabless.com>
2020-01-27 13:52:15 -0800

    flash startup edits

--- test/raptor_flash.py
+++ test/raptor_flash.py
@@ -1,9 +1,7 @@
 from pyftdi.ftdi import Ftdi
 
-Ftdi().open_mpsee_from_url('ftdi://ftdi:232h:2:6/1')
-print(Ftdi().ic_name())
-print(Ftdi().device_version())
-print(Ftdi().has_mpsse())
-print(Ftdi().is_mpsse())
+Ftdi().open_mpsse_from_url('ftdi://ftdi:232h:2:6/1')
+print(Ftdi().has_mpsse)
+print(Ftdi().is_mpsse)
 
 Ftdi().close()
