126302d8073018f6a4729193bc754b8bac51a31a
jeffdi <jeffdi@efabless.com>
2020-01-27 14:36:58 -0800

    raptor_flash.py added

--- test/raptor_flash.py
+++ test/raptor_flash.py
@@ -8,12 +8,12 @@ spi = SpiController(cs_count=1, turbo=True)
 # spi.configure(vendor=0x0403, product=0x6014, interface=1)
 spi.configure('ftdi://::/1')
 slave = spi.get_port(cs=0, freq=12E6, mode=0)  # Chip select is 0 -- corresponds to D3
+
 jedec_id = slave.exchange([0x9f], 3)
-print(binascii.hexlify(jedec_id))
+print("JEDEC = " + binascii.hexlify(jedec_id))
+
+slave.write(0x06)
+slave.write(0x60)
+
 spi.terminate()
 
-# acbus_direction = 0x01 # Bit C0 is output for reset
-# ctrl._ftdi.write_data(Array('B', [Ftdi.SET_BITS_HIGH, 0x00, acbus_direction]))
-# time.sleep(1.0)
-# ctrl._ftdi.write_data(Array('B', [Ftdi.SET_BITS_HIGH, 0x01, acbus_direction]))
-# time.sleep(1.0)
