84353d1e4d715ede18d39f3f53c8ab88352bf232
jeffdi <jeffdi@efabless.com>
2020-01-27 14:03:40 -0800

    raptor_flash.py added

--- test/raptor_flash.py
+++ test/raptor_flash.py
@@ -3,10 +3,9 @@ import time
 from pyftdi.spi import SpiController
 from array import array as Array
 
-ctrl = SpiController(cs_count=1, turbo=True)
-ctrl.configure(vendor=0x0403, product=0x6014, interface=1)
-spi = ctrl.get_port(cs=0)  # Chip select is 0 -- corresponds to D3
-spi.set_frequency(3000000)
+spi = SpiController(cs_count=1, turbo=True)
+spi.configure(vendor=0x0403, product=0x6014, interface=1)
+slave = spi.get_port(cs=0, freq=12E6, mode=0)  # Chip select is 0 -- corresponds to D3
 
 # acbus_direction = 0x01 # Bit C0 is output for reset
 # ctrl._ftdi.write_data(Array('B', [Ftdi.SET_BITS_HIGH, 0x00, acbus_direction]))
