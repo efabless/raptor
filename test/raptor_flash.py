2f5c012d8bcad051c6c42ebc9221843a956ed2d3
jeffdi <jeffdi@efabless.com>
2020-01-27 18:04:30 -0800

    raptor_flash.py added

--- test/raptor_flash.py
+++ test/raptor_flash.py
@@ -82,8 +82,9 @@ slave.write([CMD_RESET_CHIP])
 jedec_id = slave.exchange([CMD_JEDEC_DATA], 3)
 print("JEDEC = {}".format(binascii.hexlify(jedec_id)))
 
-if jedec_id[0] != b'0xef':
+if jedec_id[0:1] != bytes.fromhex('ef'):
     print("Winbond SRAM not found")
+    print(jedec_id[0:1])
     sys.exit()
 
 print("status = 0x{}".format(get_status(slave), '02x'))
