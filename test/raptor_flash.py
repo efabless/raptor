b94185c4f2f4d300a09a8125a2887b5210575763
jeffdi <jeffdi@efabless.com>
2020-01-27 16:46:16 -0800

    raptor_flash.py added

--- test/raptor_flash.py
+++ test/raptor_flash.py
@@ -4,6 +4,52 @@ from pyftdi.spi import SpiController
 from array import array as Array
 import binascii
 
+
+SR_WIP = 0b00000001  # Busy/Work-in-progress bit
+SR_WEL = 0b00000010  # Write enable bit
+SR_BP0 = 0b00000100  # bit protect #0
+SR_BP1 = 0b00001000  # bit protect #1
+SR_BP2 = 0b00010000  # bit protect #2
+SR_BP3 = 0b00100000  # bit protect #3
+SR_TBP = SR_BP3      # top-bottom protect bit
+SR_SP = 0b01000000
+SR_BPL = 0b10000000
+SR_PROTECT_NONE = 0  # BP[0..2] = 0
+SR_PROTECT_ALL = 0b00011100  # BP[0..2] = 1
+SR_LOCK_PROTECT = SR_BPL
+SR_UNLOCK_PROTECT = 0
+SR_BPL_SHIFT = 2
+
+CMD_READ_STATUS = 0x05  # Read status register
+CMD_WRITE_ENABLE = 0x06  # Write enable
+CMD_WRITE_DISABLE = 0x04  # Write disable
+CMD_PROGRAM_PAGE = 0x02  # Write page
+CMD_EWSR = 0x50  # Enable write status register
+CMD_WRSR = 0x01  # Write status register
+CMD_ERASE_SUBSECTOR = 0x20
+CMD_ERASE_HSECTOR = 0x52
+CMD_ERASE_SECTOR = 0xD8
+# CMD_ERASE_CHIP = 0xC7
+CMD_ERASE_CHIP = 0x60
+
+JEDEC_ID = 0xEF
+DEVICES = {0x30: 'W25X', 0x40: 'W25Q'}
+SIZES = {0x11: 1 << 17, 0x12: 1 << 18, 0x13: 1 << 19, 0x14: 1 << 20,
+         0x15: 2 << 20, 0x16: 4 << 20, 0x17: 8 << 20, 0x18: 16 << 20}
+SPI_FREQ_MAX = 104  # MHz
+CMD_READ_UID = 0x4B
+UID_LEN = 0x8  # 64 bits
+READ_UID_WIDTH = 4  # 4 dummy bytes
+TIMINGS = {'page': (0.0015, 0.003),  # 1.5/3 ms
+           'subsector': (0.200, 0.200),  # 200/200 ms
+           'sector': (1.0, 1.0),  # 1/1 s
+           'bulk': (32, 64),  # seconds
+           'lock': (0.05, 0.1),  # 50/100 ms
+           'chip': (4, 11)}
+# FEATURES = (SerialFlash.FEAT_SECTERASE |
+#             SerialFlash.FEAT_SUBSECTERASE |
+#             SerialFlash.FEAT_CHIPERASE)
+
 spi = SpiController(cs_count=1, turbo=True)
 # spi.configure(vendor=0x0403, product=0x6014, interface=1)
 spi.configure('ftdi://::/1')
@@ -12,8 +58,14 @@ slave = spi.get_port(cs=0, freq=12E6, mode=0)  # Chip select is 0 -- corresponds
 jedec_id = slave.exchange([0x9f], 3)
 print("JEDEC = {}".format(binascii.hexlify(jedec_id)))
 
-slave.write([0x06])
-slave.write([0x60])
+status = slave.exchange(bytes(CMD_READ_STATUS),1)
+print("status = {}".format(binascii.hexlify(status)))
+
+slave.write(bytes(CMD_WRITE_ENABLE))
+slave.write(bytes(CMD_ERASE_CHIP))
+
+status = slave.exchange(bytes(CMD_READ_STATUS),1)
+print("status = {}".format(binascii.hexlify(status)))
 
 spi.terminate()
 
