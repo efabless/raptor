b8dbded1915259de1ebc489f8d98d56c618b03c8
jeffdi <jeffdi@efabless.com>
2020-02-10 17:47:00 -0800

    fix blink.c

--- firmware/blink/blink.c
+++ firmware/blink/blink.c
@@ -50,32 +50,32 @@ void main()
 //	CLKCTRL_PLLCR =
 //	CLKCTRL_PLLTR =
 
-    *(0x80000004) = (unsigned int) 0x0000;
-    *(0x80000000) = (unsigned int) 0x000f;
+//    *(0x80000004) = (unsigned int) 0x0000;
+//    *(0x80000000) = (unsigned int) 0x000f;
 
 	// Enable GPIO (all output, ena = 0)
-//	gpio_set_dir(0x0000);
-//    gpio_write(0x000f);
+	gpio_set_dir(0x0000);
+    gpio_write(0x000f);
 
     for (j = 0; j < 170000; j++);
 
     while (1) {
-//        gpio_write(0x0000);
-//        for (j = 0; j < 70000; j++);
-//        gpio_write(0x0001);
-//        for (j = 0; j < 70000; j++);
-//        gpio_write(0x0002);
-//        for (j = 0; j < 70000; j++);
-//        gpio_write(0x0004);
-//        for (j = 0; j < 70000; j++);
-//        gpio_write(0x0008);
-//        for (j = 0; j < 70000; j++);
-//        gpio_write(0x0004);
-//        for (j = 0; j < 70000; j++);
-//        gpio_write(0x0002);
-//        for (j = 0; j < 70000; j++);
-//        gpio_write(0x0001);
-//        for (j = 0; j < 70000; j++);
+        gpio_write(0x0000);
+        for (j = 0; j < 70000; j++);
+        gpio_write(0x0001);
+        for (j = 0; j < 70000; j++);
+        gpio_write(0x0002);
+        for (j = 0; j < 70000; j++);
+        gpio_write(0x0004);
+        for (j = 0; j < 70000; j++);
+        gpio_write(0x0008);
+        for (j = 0; j < 70000; j++);
+        gpio_write(0x0004);
+        for (j = 0; j < 70000; j++);
+        gpio_write(0x0002);
+        for (j = 0; j < 70000; j++);
+        gpio_write(0x0001);
+        for (j = 0; j < 70000; j++);
     }
 
 }
